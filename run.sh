1e9ba5eb4791e67cb9720cab28d3b6ac09f5f780
Vahid Spacer <108230053+Vahid-Spacer@users.noreply.github.com>
2024-06-01 12:49:31 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,11 +22,11 @@ if [ "$choices" -eq 1 ]; then
   rctext='#!/bin/bash
 
 ip tunnel add 6to4tun_IR mode sit remote '"$ip_remote"' local '"$ipv4_address"'
-ip -6 addr add 2001:470:1f10:e1f::1/64 dev 6to4tun_IR
+ip -6 addr add fe80:470:1f10:e1f::1/64 dev 6to4tun_IR
 ip link set 6to4tun_IR mtu 1480
 ip link set 6to4tun_IR up
 # confige tunnele GRE6 ya IPIPv6 IR
-ip -6 tunnel add GRE6Tun_IR mode ip6gre remote 2001:470:1f10:e1f::2 local 2001:470:1f10:e1f::1
+ip -6 tunnel add GRE6Tun_IR mode ip6gre remote fe80:470:1f10:e1f::2 local fe80:470:1f10:e1f::1
 ip addr add 172.16.1.1/30 dev GRE6Tun_IR
 ip link set GRE6Tun_IR mtu 1436
 ip link set GRE6Tun_IR up
@@ -60,11 +60,11 @@ elif [ "$choices" -eq 2 ]; then
   read -p "enter Iran Ip : " ip_remote
   rctext='#!/bin/bash
 ip tunnel add 6to4tun_KH mode sit remote '"$ip_remote"' local '"$ipv4_address"'
-ip -6 addr add 2001:470:1f10:e1f::2/64 dev 6to4tun_KH
+ip -6 addr add fe80:470:1f10:e1f::2/64 dev 6to4tun_KH
 ip link set 6to4tun_KH mtu 1480
 ip link set 6to4tun_KH up
 
-ip -6 tunnel add GRE6Tun_KH mode ip6gre remote 2001:470:1f10:e1f::1 local 2001:470:1f10:e1f::2
+ip -6 tunnel add GRE6Tun_KH mode ip6gre remote fe80:470:1f10:e1f::1 local fe80:470:1f10:e1f::2
 ip addr add 172.16.1.2/30 dev GRE6Tun_KH
 ip link set GRE6Tun_KH mtu 1436
 ip link set GRE6Tun_KH up
@@ -111,7 +111,7 @@ if [[ "$choices" -eq 1 || "$choices" -eq 2 ]]; then
   echo    # move to a new line
 
   if [ "$choices" -eq 2 ]; then
-  echo "Local IPv6 Kharej: 2001:470:1f10:e1f::2"
-  echo "Local Ipv6 Iran: 2001:470:1f10:e1f::1"
+  echo "Local IPv6 Kharej: fe80:470:1f10:e1f::2"
+  echo "Local Ipv6 Iran: fe80:470:1f10:e1f::1"
   fi
 fi
